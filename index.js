b4868198f71de0a7640c272da67e815391ae4b55
varvarvarvara <varya-rozina@yandex.ru>
2021-11-08 12:39:37 +0300

    add validation

--- index.js
+++ index.js
@@ -9,45 +9,56 @@ function calculate () {
 
     targetDiv.appendChild(text);
 
-    const salary = +document.getElementById("salary").value;
-    const tax = salary * 12 * 0.13;
-    let taxCounter = 0;    
-    let counter = 1;
-
-    while (taxCounter < 260000) {
-        let newRow = document.createElement("div");
-        newRow.className = "row";
-
-        let newCol = document.createElement("div");
-        newCol.className = "col";
-
-        let newCheckbox = document.createElement("input");
-        newCheckbox.type = "checkbox";
-        newCheckbox.id = counter + "-check";
-        newCheckbox.className = "newCheckbox"
-
-        let newLabel = document.createElement("label");
-        newLabel.htmlFor = newCheckbox.id;
-        newLabel.className = "newLabel";
-
-        if (taxCounter + tax >= 260000) {
-            let lastTax = 260000 - taxCounter;
-            newLabel.innerHTML = lastTax + " рублей <span> в " + counter + " год </span>";
-        } else {
-            newLabel.innerHTML = tax + " рублей <span> в " + counter + " год </span>";
+    try {
+        const salary = +document.getElementById("salary").value;
+        if (salary == undefined || salary == NaN || salary == 0){
+            throw new Error("вы не ввели значение")
         }
-
-        targetDiv.appendChild(newRow);
-
-        newRow.appendChild(newCol);
-
-        newCol.appendChild(newCheckbox);
-
-        newCol.appendChild(newLabel);
-
-
-
-        counter++;
-        taxCounter += tax;
+        const tax = salary * 12 * 0.13;
+        let taxCounter = 0;    
+        let counter = 1;
+    
+        while (taxCounter < 260000) {
+            let newRow = document.createElement("div");
+            newRow.className = "row";
+    
+            let newCol = document.createElement("div");
+            newCol.className = "col";
+    
+            let newCheckbox = document.createElement("input");
+            newCheckbox.type = "checkbox";
+            newCheckbox.id = counter + "-check";
+            newCheckbox.className = "newCheckbox"
+    
+            let newLabel = document.createElement("label");
+            newLabel.htmlFor = newCheckbox.id;
+            newLabel.className = "newLabel";
+    
+            if (taxCounter + tax >= 260000) {
+                let lastTax = 260000 - taxCounter;
+                newLabel.innerHTML = lastTax + " рублей <span> в " + counter + " год </span>";
+            } else {
+                newLabel.innerHTML = tax + " рублей <span> в " + counter + " год </span>";
+            }
+    
+            targetDiv.appendChild(newRow);
+    
+            newRow.appendChild(newCol);
+    
+            newCol.appendChild(newCheckbox);
+    
+            newCol.appendChild(newLabel);
+    
+    
+    
+            counter++;
+            taxCounter += tax;
+        }        
+    } catch (error) {
+        console.log("🚀 ~ file: index.js ~ line 58 ~ calculate ~ error", error)
+        alert(error.message)
+        
     }
+
+
 }
